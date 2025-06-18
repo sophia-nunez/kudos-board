a9bfae6e5fc0e308b0248aa2601f1a4322b8b342
Sophia Rose Nunez <sophiarnunez@hotmail.com>
2025-06-17 22:45:03 -0700

    Updated frontend to fetch from database

--- frontend/src/components/CreateModal.jsx
+++ frontend/src/components/CreateModal.jsx
@@ -1,33 +1,98 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/Modal.css";
 import { fetchGifs, searchGifs } from "../utils";
 import GifSelect from "./GifSelect";
+import { createBoard, createCard } from "../utils";
 
-const CreateModal = ({ reference }) => {
+const CreateModal = ({ modalType, reference, setModalOpen }) => {
   const [selectedGif, setSelectedGif] = useState(
     "https://giphy.com/embed/tFSqMSMnzPRTAdvKyr"
   );
+  const [formInput, setFormInput] = useState({
+    title: "",
+    description: "",
+    author: "",
+    imageURL: selectedGif,
+    altText: "GIF for board cover",
+  });
 
+  useEffect(() => {
+    setFormInput((prev) => ({
+      ...prev,
+      imageURL: selectedGif,
+    }));
+  }, [selectedGif]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (modalType === "create-board") {
+      createBoard(formInput);
+    } else {
+      createCard(formInput);
+    }
+
+    setFormInput({
+      title: "",
+      description: "",
+      author: "",
+      imageURL: selectedGif,
+      altText: "",
+    });
+
+    setModalOpen(false);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormInput((prevData) => ({
+      ...prevData, // keep data but replace target value
+      [name]: value,
+    }));
+  };
+
+  // then send push request
   return (
     <aside id="create-modal" className="modal" ref={reference}>
       <span className="close">&times;</span>
       <section id="modal-content">
-        <form id="create-form">
+        <form id="create-form" onSubmit={handleSubmit}>
           <div>
             <label htmlFor="name">Title: </label>
-            <input type="text" id="name" name="name" required />
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={formInput.title}
+              onChange={handleChange}
+              required
+            />
           </div>
           <div>
-            <label htmlFor="image">Select Cover GIF: </label>
+            <label>Select Cover GIF: </label>
             <GifSelect setSelectedGif={setSelectedGif} />
           </div>
           <div>
             <label htmlFor="description">Description: </label>
-            <input type="text" id="description" name="description" required />
+            <input
+              type="text"
+              id="description"
+              name="description"
+              value={formInput.description}
+              onChange={handleChange}
+              required
+            />
           </div>
           <div>
             <label htmlFor="author">Author (optional): </label>
-            <input type="text" id="author" name="author" />
+            <input
+              type="text"
+              id="author"
+              name="author"
+              value={formInput.author}
+              onChange={handleChange}
+            />
           </div>
 
           <button type="submit">Create Board</button>
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,15 +1,15 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 const giphyURL = "https://api.giphy.com/v1/gifs/";
+const boardURL = "http://localhost:3000";
 
 const fetchBoards = async () => {
   // get data here, mock data for now
   try {
-    const response = await fetch("../data.json");
+    const response = await fetch(boardURL);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    const data = await response.json();
-    const boards = data.kudosBoards;
+    const boards = await response.json();
     return boards;
   } catch (error) {
     console.error("Failed to load kudos boards:", error); // get rid of after dev
@@ -18,12 +18,26 @@ const fetchBoards = async () => {
 };
 
 const fetchBoardById = async (id) => {
-  const boards = await fetchBoards();
-  const fetchedBoard = boards[id];
+  try {
+    const response = await fetch(`${boardURL}/board/${id}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    let board = await response.json();
 
-  return fetchedBoard;
+    // get cards
+    const res = await fetch(`${boardURL}/board/${id}/cards`);
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const cards = await res.json();
 
-  // TODO change fetch request to directly call the board id (once api setup)
+    board = { ...board, cards: cards };
+    return board;
+  } catch (error) {
+    console.error(`Failed to load board: ${id}`, error); // get rid of after dev
+    return {};
+  }
   // TODO add in error handling as needed
 };
 
@@ -59,4 +73,34 @@ const searchGifs = async (query) => {
   }
 };
 
-export { fetchBoards, fetchBoardById, fetchGifs, searchGifs };
+const createBoard = async (formInput) => {
+  try {
+    console.log(JSON.stringify(formInput));
+    const response = await fetch(boardURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formInput),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Failed to create board:", error); // get rid of after dev
+  }
+  console.log("board created");
+};
+
+const createCard = (formInput) => {
+  console.log("card created");
+};
+
+export {
+  fetchBoards,
+  fetchBoardById,
+  fetchGifs,
+  searchGifs,
+  createBoard,
+  createCard,
+};
