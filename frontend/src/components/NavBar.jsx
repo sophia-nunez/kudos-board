ecc001655016a2fd9235915aff17c1be8bf8723b
Sophia Rose Nunez <sophiarnunez@hotmail.com>
2025-06-19 23:49:44 -0700

    Added conditional styling to selected filter button

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -36,19 +36,39 @@ const NavBar = ({
         handleSearch={loadPage}
       />
       <div className="filter-container">
-        <button id="all" onClick={onFilter}>
+        <button
+          id="all"
+          className={filter === "all" ? "selected" : ""}
+          onClick={onFilter}
+        >
           All
         </button>
-        <button id="Recent" onClick={onFilter}>
+        <button
+          id="Recent"
+          className={filter === "Recent" ? "selected" : ""}
+          onClick={onFilter}
+        >
           Recent
         </button>
-        <button id="Celebration" onClick={onFilter}>
+        <button
+          id="Celebration"
+          className={filter === "Celebration" ? "selected" : ""}
+          onClick={onFilter}
+        >
           Celebration
         </button>
-        <button id="ThankYou" onClick={onFilter}>
+        <button
+          id="ThankYou"
+          className={filter === "Thank you" ? "selected" : ""}
+          onClick={onFilter}
+        >
           Thank You
         </button>
-        <button id="Inspiration" onClick={onFilter}>
+        <button
+          id="Inspiration"
+          className={filter === "Inspiration" ? "selected" : ""}
+          onClick={onFilter}
+        >
           Inspiration
         </button>
       </div>
