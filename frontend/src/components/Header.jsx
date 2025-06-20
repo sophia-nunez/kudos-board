d6e8178c6caf40404d9b7adc29c6b7a731eae475
Sophia Rose Nunez <sophiarnunez@hotmail.com>
2025-06-20 10:05:14 -0700

    General styling for board page and small screen responsiveness

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,13 @@
-import '../styles/Header.css'
+import logo from "../assets/images/kudos.png";
+import "../styles/Header.css";
 
 const Header = () => {
-
   return (
     <header>
+      <img src={logo} alt="Kudosboard logo" className="logo" />
       <h1>KUDOBOARD</h1>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
